5234b91960b79e666d8b24ddfb4a6d8765b31427
Nikita Chulkov <chulkovnd@yandex.ru>
2022-12-05 19:37:14 +0300

    task 19 done

---
+++ seminar3/task19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите пятизначное число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+while (num1 / 10000 > 10 || num1 / 10000 <1)
+{
+    Console.Write("Введите натуральное пятизначное число: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+}
+double check1 = Math.Floor(num1 / 10000);
+double check2 = Math.Floor((Math.Floor(num1 / 1000) % 10));
+double check3 = Math.Floor((num1 % 100)/10);
+double check4 = num1 % 10;
+if (check1 == check4 || check2 == check3) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
