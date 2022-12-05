38eb3c95fa9f4e8cee0f6c18905ad76a2d929f2b
Nikita Chulkov <chulkovnd@yandex.ru>
2022-12-05 19:39:59 +0300

    task 29 done

---
+++ seminar4/task29/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int SIZE = 8;
+int [] arr = new int [SIZE];
+
+Console.WriteLine($"Введите {SIZE} чисел:");
+
+for (int i = 0; i < SIZE; i++)
+{
+    arr [i] = Convert.ToInt32(Console.ReadLine());
+    if (i == SIZE - 1 ) {
+        Console.Write("[");
+        for ( int count = 0; count < SIZE - 1 ; count++)
+         {
+            Console.Write($"{arr[count]},");
+         };
+         Console.Write($"{arr[SIZE - 1]}]");
+
+    };
+};
+
+
