6c45fe0242875c0131c7bf9432844236dd229ff7
Nikita Chulkov <chulkovnd@yandex.ru>
2022-12-05 19:39:39 +0300

    task 27 done

---
+++ seminar4/task27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0; 
+double answer = 0;
+ while ( Math.Floor(( (num % Math.Pow(10, count + 1 )) / Math.Pow(10, count ))) > 0)
+{
+    answer += Math.Floor(( (num % Math.Pow(10, count + 1 )) / Math.Pow(10, count ))) ;
+    count ++;
+  if (Math.Floor(( (num % Math.Pow(10, count + 1 )) / Math.Pow(10, count ))) == 0) Console.WriteLine(answer);
+}
